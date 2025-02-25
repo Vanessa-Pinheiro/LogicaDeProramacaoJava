f1381ec2ff327011e59ffd24cc19cd06c42fc17b
Vanessa-Pinheiro <vanessa05pinheiro@gmail.com>
2025-02-25 16:50:46 -0300

    Exercicios 06 - 10.1

--- src/ExerciciosJavaBasico/ExConversao.java
+++ src/ExerciciosJavaBasico/ExConversao.java
@@ -1,4 +1,13 @@
 package ExerciciosJavaBasico;
 
+import java.util.Scanner;
+
 public class ExConversao {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite quantos metros possui o objeto: ");
+        double metros = scan.nextDouble();
+        double conversao = metros * 100;
+        System.out.println("O objeto possui " + conversao + " cm.");
+    }
 }
